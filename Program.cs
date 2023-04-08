f1f950965678bd72f038d371d310b1e8478c1b65
fcortes <franciscojaviercortes@gmail.com>
2023-04-08 11:27:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circleAreaAndCircumference
+{
+    //Nombre del estudiante: Francisco Cortes
+    //Grupo: 268
+    //4: Calculo de area y cirferencia (perimetro) de un circulo
+    //Código Fuente: autoría propia
+    class circle
+    {
+        // variables privadas con propiedades del circulo
+        private double radius;
+
+        // propiedades accesible de acceso publico
+        public double Radius
+        {
+            get
+            {
+                return radius; // retorna nombre 
+            }
+            set
+            {
+                radius = value; // guarda nombre
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
